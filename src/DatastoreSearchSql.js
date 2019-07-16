a41c7c33e42bbf5635b3a06b80445af31d9b9846
anuveyatsu <anuar.ustayev@gmail.com>
2019-07-16 17:37:39 +0600

    [query builder][s]: generate SQL statement which includes row count

--- src/DatastoreSearchSql.js
+++ src/DatastoreSearchSql.js
@@ -12,6 +12,22 @@ function DatastoreSearchSql(props) {
   function handleSubmit(event) {
     event.preventDefault();
     // Convert query to SQL string
+    let sqlQueryString = `SELECT COUNT(*) OVER (), * FROM "${props.resourceId}" WHERE `
+    if (query.rules.length === 0) { // No rules given so alert about that
+      alert('Please, provide at least one rule.')
+    } else {
+      const combinator = query.combinator
+      query.rules.forEach((rule, index) => {
+        if (index === 0) {
+          // TODO: unquote value for numbers
+          sqlQueryString += `"${rule.field}" ${rule.operator} '${rule.value}'`
+        } else { // If we have >1 rule we will need 'AND', 'OR' combinators
+          sqlQueryString += ` ${combinator.toUpperCase()} "${rule.field}" ${rule.operator} '${rule.value}'`
+        }
+      })
+      // Set a limit of 100 rows
+      sqlQueryString += ` LIMIT 100`
+    }
     // Build a datastore URL with SQL string
     // Fetch data and update datapcakge json
     // QUESTION: is it ok to get DOM element here and update its property?
