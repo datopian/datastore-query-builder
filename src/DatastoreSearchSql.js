43745ce79f09825e6bf249ea45e1c7d542fcafe1
anuveyatsu <anuar.ustayev@gmail.com>
2019-07-19 14:32:17 +0600

    [datepicker][m]: implemented date picker with date fields + small bug fixes - refs #1

--- src/DatastoreSearchSql.js
+++ src/DatastoreSearchSql.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {Formik, Form, FieldArray, Field} from 'formik'
+import DatePicker from 'react-date-picker'
 
 
 function DatastoreSearchSql(props) {
+
+  const dateField = props.fields.find(field => field.type && field.type.includes('date'))
+  const otherFields = props.fields.filter(field => !(field.type && field.type.includes('date')))
 
   const operators = [
     {name: '=', label: '='},
@@ -14,13 +18,25 @@ function DatastoreSearchSql(props) {
   ]
 
   function handleSubmit(values) {
+    const clonedValues = JSON.parse(JSON.stringify(values))
     // Convert query to SQL string. Note we're adding 'COUNT(*) OVER()' so that
     // we get number of total rows info.
     let sqlQueryString = `SELECT COUNT(*) OVER (), * FROM "${props.resourceId}" WHERE `
-    if (values.rules.length === 0) { // No rules given so alert about that
+    if (!clonedValues.startDate && !clonedValues.endDate && clonedValues.rules.length === 0) { // No filters given so alert about that
       alert('Please, provide at least one rule.')
     } else {
-      values.rules.forEach((rule, index) => {
+      if (clonedValues.startDate) {
+        const rule = {combinator: 'AND', field: dateField.name, operator: '>=', value: clonedValues.startDate}
+        clonedValues.rules.push(rule)
+      }
+      if (clonedValues.endDate) {
+        const rule = {combinator: 'AND', field: dateField.name, operator: '<=', value: clonedValues.endDate}
+        clonedValues.rules.push(rule)
+      }
+
+      clonedValues.rules.forEach((rule, index) => {
+        // Convert JS date object into string:
+        rule.value = rule.value instanceof Date ? rule.value.toISOString() : rule.value
         if (index === 0) {
           // TODO: unquote value for numbers
           sqlQueryString += `"${rule.field}" ${rule.operator} '${rule.value}'`
@@ -39,7 +55,7 @@ function DatastoreSearchSql(props) {
       .then(data => {
         // Records come with 'count', '_id' and '_full_text' columns from datastore
         // so we need to delete them. But first save 'count' value.
-        const count = data.result.records[0].count
+        const count = data.result.records[0] ? data.result.records[0].count : 0
         // Update number of rows:
         document.getElementById('numberOfRows').innerText = count
         const newData = data.result.records.map(record => {
@@ -61,12 +77,33 @@ function DatastoreSearchSql(props) {
 
   return (
     <Formik
-      initialValues={{ rules: [] }}
+      initialValues={{ rules: [], startDate: null, endDate: null }}
       onSubmit={values =>
         handleSubmit(values)
       }
-      render={({ values }) => (
-        <Form>
+      render={({ values, setFieldValue }) => (
+        <Form className="form-inline">
+          {dateField ? (
+            <div>
+              <DatePicker
+                value={values.startDate}
+                clearIcon='X'
+                onChange={val => setFieldValue(`startDate`, val)}
+                format='yyyy-MM-dd'
+                maxDate={new Date()} />
+              <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+              <DatePicker
+                  value={values.endDate}
+                  clearIcon='X'
+                  onChange={val => setFieldValue(`endDate`, val)}
+                  returnValue='end'
+                  format='yyyy-MM-dd'
+                  minDate={values.startDate}
+                  maxDate={new Date()} />
+            </div>
+          ) : (
+            ''
+          )}
           <FieldArray
             name='rules'
             render={arrayHelpers => (
@@ -74,43 +111,45 @@ function DatastoreSearchSql(props) {
                 {values.rules && values.rules.length > 0 ? (
                   values.rules.map((rule, index) => (
                     <div key={index}>
-                      <Field name={`rules.${index}.combinator`} component="select" required>
+                      <Field name={`rules.${index}.combinator`} component="select" className="form-control" required>
                         <option value="AND">AND</option>
                         <option value="OR">OR</option>
                       </Field>
-                      <Field name={`rules.${index}.field`} component="select" required>
-                        {props.fields.map((field, index) => (
+                      <Field name={`rules.${index}.field`} component="select" className="form-control" required>
+                        {otherFields.map((field, index) => (
                           <option value={field.name} key={`field${index}`}>{field.title || field.name}</option>
                         ))}
                       </Field>
-                      <Field name={`rules.${index}.operator`} component="select" required>
+                      <Field name={`rules.${index}.operator`} component="select" className="form-control" required>
                         {operators.map((operator, index) => (
                           <option value={operator.name} key={`operator${index}`}>{operator.label}</option>
                         ))}
                       </Field>
-                      <Field name={`rules.${index}.value`} required />
+                      <Field name={`rules.${index}.value`} className="form-control" required />
                       <button
                         type="button"
+                        className="btn btn-default"
                         onClick={() => arrayHelpers.remove(index)} // remove a rule from the list
                       >
                         -
                       </button>
                       <button
                         type="button"
-                        onClick={() => arrayHelpers.insert(index, {combinator: 'AND', field: props.fields[0].name, operator: '=', value: ''})} // insert an empty rule at a position
+                        className="btn btn-default"
+                        onClick={() => arrayHelpers.insert(index, {combinator: 'AND', field: otherFields[0].name, operator: '=', value: ''})} // insert an empty rule at a position
                       >
                         +
                       </button>
                     </div>
                   ))
                 ) : (
-                  <button type="button" onClick={() => arrayHelpers.push({combinator: 'AND', field: props.fields[0].name, operator: '=', value: ''})}>
+                  <button type="button" className="btn btn-default" onClick={() => arrayHelpers.push({combinator: 'AND', field: otherFields[0].name, operator: '=', value: ''})}>
                     {/* show this when user has removed all rules from the list */}
                     Add a rule
                   </button>
                 )}
                 <div>
-                  <button type="submit">Submit</button>
+                  <button type="submit" className="btn btn-primary">Submit</button>
                 </div>
               </div>
             )}
