40b23724a91a7f82735482bcbea89949647c824e
anuveyatsu <anuar.ustayev@gmail.com>
2019-12-06 14:17:15 +0600

    [v0.3.3]: fixed dummy test so that github actions successes.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {QueryBuilder} from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<QueryBuilder resource={{}} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
